26990f1f1c245958a5387bf24ad5fac20f1b48e7
Bernie Pope <florbitous@gmail.com>
2014-11-26 14:00:38 +1100

    Added graph label and refactored code

--- plot_enrolments.py
+++ plot_enrolments.py
@@ -7,7 +7,7 @@ time, based on data downloaded from the University of Melbourne LMS system.
 The LMS allows you to download a history of student enrolments as a CSV file.
 Rows in the file have the format:
 
-    DATE,STUDENT_NAME,STUDENT_NUMBER,STUDENT_USERNAME,ACTION
+    DATE,LAST_NAME,GIVEN_NAMES,STUDENT_NUMBER,USERNAME,ACTION
 
 Dates are in the format:
 
@@ -30,7 +30,8 @@ days from the epoch. Days before the epoch are negative and days after
 the epoch are positive.
 
 usage: plot_enrolments.py [-h] --epoch EPOCH [--output OUTPUT] [--title TITLE]
-                          [--low LOW] [--high HIGH]
+                          [--low LOW] [--high HIGH] [--label]
+                          [--labelx LABELX] [--labely LABELY]
                           csv_filename
 
 Plot a graph of student enrolments given CSV file downloaded from the LMS
@@ -47,16 +48,22 @@ optional arguments:
   --title TITLE    Title text for output graph
   --low LOW        Lower bound of days before epoch to start counting
   --high HIGH      Upper bound of days before epoch to end counting
+  --label          Annotate the graph with a label on the maximum and current
+                   enrolment numbers
+  --labelx LABELX  X position for label
+  --labely LABELY  Y position for label
 
-Example:
+Command line example:
 
-plot_enrolments.py --epoch '28-JUL-2014' --title "MySubject Semester 2 2014" \
-                --output enrolments.png --low 28 enrolments.csv 
+   ./plot_enrolments.py --epoch '28-JUL-1933' --title "My Subject Semester 2 2014" \
+                        --output enrolments.png --low 28 \
+                        --label --labelx 30 --labely '160' fake_enrolments.csv
 
-In the example above, the semester starts on 28-JUL-2014, which is given
-as the "epoch". The title of the graph will be "MySubject Semester 2 2014".
+In the example above, the semester starts on 28-JUL-1933, which is given
+as the "epoch". The title of the graph will be "My Subject Semester 2 1933".
 The output file will be written to "enrolments.png". The graph will be
-cut off at the lower end 28 days before the epoch.
+cut off at the lower end 28 days before the epoch. The graph will be labelled
+with the maximum and current enrolments positioned at (30, 160).
 '''
 
 import matplotlib.pyplot as plt
@@ -73,7 +80,11 @@ DEFAULT_LOW_BOUND = 50
 DEFAULT_HIGH_BOUND = 100
 SECONDS_PER_DAY = 60 * 60 * 24
 LAST_MINUTE_OF_DAY = '23:59'
+NUM_FIELDS = 6
 
+# Representation of one row from the input CSV file.
+# We only care about the time of the event and what kind
+# of action it was: Removed or Added.
 Record = namedtuple('Record', ['time', 'action'])
 
 def parse_args():
@@ -90,63 +101,92 @@ def parse_args():
         help='Upper bound of days before epoch to end counting')
     parser.add_argument('csv_filename', type=str, 
         help='CSV file downloaded from the LMS')
+    parser.add_argument('--label', action='store_true',
+        help='Annotate the graph with a label on the maximum and current enrolment numbers')
+    parser.add_argument('--labelx', type=float,
+            default=30,
+            help="X position for label")
+    parser.add_argument('--labely', type=float,
+            default=30,
+            help="Y position for label")
     return parser.parse_args()
 
-def plot_data(days, num_students, title, output_filename):
-    plt.ylabel("number of enrolled students")
-    plt.xlabel("day")
-    plt.title(title)
+def plot_data(days, num_students, args):
+    plt.ylabel("Number of enrolled students")
+    plt.xlabel("Days from {}".format(args.epoch))
+    plt.title(args.title)
     plt.plot(days, num_students)
-    plt.savefig(output_filename)
+    # Optionally display a label showing the maximum and
+    # current enrolment.
+    if args.label and num_students:
+        maximum_enrolment = max(num_students)
+        current_enrolment = num_students[-1]
+        annotation = "max = {}\ncurrent = {}" \
+                        .format(maximum_enrolment, current_enrolment)
+        position = (args.labelx, args.labely)
+        plt.annotate(annotation, xy=position, xycoords='data')
+    plt.savefig(args.output)
     plt.close()
 
 def parse_date_time(date_time_str):
+    '''Parse a date string into a float. Presumably the float
+    is some system dependent value in seconds since some. We don't
+    care about its absolute value. We only care that it can be
+    compared to other dates.'''
     return time.mktime(time.strptime(date_time_str, DATE_FORMAT))
 
 def days_difference(time_recent, time_older):
     '''Most recent time is specified first'''
     return int((time_recent - time_older) / SECONDS_PER_DAY)
 
-def main():
-    args = parse_args()
+def process_records(args, records):
+    num_students = 0
+    histogram = {}
+    # set the epoch time to the last minute of the epoch day
+    # XXX probably should be last second.
+    epoch = parse_date_time(args.epoch.strip() + ' ' + LAST_MINUTE_OF_DAY)
+    # Compute a running total of number of students for each day of data
+    for record in records:
+        day = days_difference(record.time, epoch)
+        if record.action == 'Added':
+            num_students += 1
+        elif record.action == 'Removed':
+            num_students -= 1
+        histogram[day] = num_students
+    # Compute a list of days and a corresponding list of students
+    # for plotting.
+    days, num_students = [], []
+    for day, num in sorted(histogram.items()):
+        if -args.low <= day <= args.high:
+            # Only plot days within the specified bounds.
+            days.append(day)
+            num_students.append(num)
+    plot_data(days, num_students, args)
+
+def read_records(csv_filename):
+    '''Read each record from the input CSV file'''
     records = []
-    with open(args.csv_filename) as csv_file:
+    with open(csv_filename) as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
-            if len(row) == 6:
+            if len(row) == NUM_FIELDS:
                 time_of_event_str, action = row[0], row[-1]
-                time_of_event = parse_date_time(time_of_event_str)
-                record = Record(time=time_of_event, action=action)
-            records.append(record)
+                if action in ['Added', 'Removed']:
+                    time_of_event = parse_date_time(time_of_event_str)
+                    record = Record(time=time_of_event, action=action)
+                    records.append(record)
+                else:
+                    print("Unrecogonised action, skipping: {}".format(row))
+    return records
 
+def main():
+    args = parse_args()
+    records = read_records(args.csv_filename)
     if records:
         # The CSV file is in reverse temporal order (youngest date on first line).
         # We reverse them to put them in normal temporal order.
         records = list(reversed(records))
-        num_students = 0
-        histogram = {}
-        epoch = parse_date_time(args.epoch.strip() + ' ' + LAST_MINUTE_OF_DAY)
-        for record in records:
-            #day = int((record.time - epoch) / SECONDS_PER_DAY)
-            day = days_difference(record.time, epoch)
-            if record.action == 'Added':
-                num_students += 1
-            elif record.action == 'Removed':
-                num_students -= 1
-            else:
-                print("Unrecogonised action, skipping record: {}".format(record))
-            histogram[day] = num_students
-        days, num_students = [], []
-        for day, num in sorted(histogram.items()):
-            if -args.low <= day <= args.high:
-                # Only plot days within the specified bounds.
-                days.append(day)
-                num_students.append(num)
-        plot_data(days, num_students, args.title, args.output)
-        max_num_students = max(num_students)
-        current_num_students = num_students[-1]
-        print("Maximum enrolled students = {}".format(max_num_students))
-        print("Current enrolled students = {}".format(current_num_students))
+        process_records(args, records)
 
 if __name__ == '__main__':
     main()
